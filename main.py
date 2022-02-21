83e07fb94f313a951ee07884badb4f1cd532db9d
Yuriy <rund2504@gmail.com>
2022-02-21 20:58:33 +0300

    Add start page

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print('Hello world!')
-name = input('Enter your name: ')
-print(f'Your name: {name}')
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return 'Hi ;)'
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
